cc92ae4f6973d59a2ce1bdfbcca8d2f864145ed8
ebao <ebao@toolsgroup.com>
2017-08-30 17:56:25 +0200

    add attention DM

--- src/datamanager.py
+++ src/datamanager.py
@@ -1,12 +1,15 @@
 from tensorflow.contrib.keras.api.keras.preprocessing.sequence import pad_sequences
 from pandas import get_dummies
 from src.utils import freq_dist, w_index
+from pandas import Series
+from numpy import int32
 
 class DataManager(object):
 
-    def __init__(self, batch_size, max_seq_len=None, sentcol='SENT', clscol='CLS', start_idx=3):
+    def __init__(self, batch_size, max_seq_len=None, aspcol='ASP', sentcol='SENT', clscol='CLS', start_idx=3):
         self.batch_size = batch_size
         self.max_seq_len = max_seq_len
+        self.aspcol = aspcol
         self.sentcol = sentcol
         self.clscol = clscol
         self.start_idx = start_idx
@@ -24,7 +27,7 @@ class DataManager(object):
     def init(self, dataset):
         """Must initialize with complete dataset"""
         _df = self.tokenize(dataset)
-        self.freq_dist = freq_dist(_df['TOKENS'])
+        self.freq_dist = freq_dist(_df['TOKENS'].append(Series([_df[self.aspcol].unique().tolist()]),True))
         self.w_idx = w_index(self.freq_dist, self.start_idx)
         self.vocab = len(self.w_idx)
         print('Longest sent length: %i' %_df['TLEN'].max())
@@ -50,11 +53,59 @@ class DataManager(object):
         self.n_batchs = size// self.batch_size if size % self.batch_size == 0 else size // self.batch_size + 1
         current = 0
         for i in range(self.n_batchs):
+            next_batch = self.batch_size * (i + 1)
             if i == self.n_batchs - 1:
                 _X = [X[t][current:] for t in range(self.max_seq_len)]
                 _y = y[current:, :]
             else:
-                _X = [X[t][current:self.batch_size * (i + 1)] for t in range(self.max_seq_len)]
-                _y = y[current:self.batch_size * (i + 1), :]
-            current = self.batch_size * (i + 1)
+                _X = [X[t][current:next_batch] for t in range(self.max_seq_len)]
+                _y = y[current:next_batch, :]
+            current = next_batch
             yield _X, _y
+
+
+class AttDataManager(DataManager):
+
+    def __init__(self, **kwargs):
+        DataManager.__init__(self, **kwargs)
+
+
+    def input_ready(self, df, tokenize=False):
+        if tokenize:
+            _df = self.tokenize(df)
+        else:
+            _df = df.copy()
+        X = _df['TOKENS'].apply(lambda x: [self.w_idx[w] for w in x]).values
+        X = pad_sequences(X, maxlen=self.max_seq_len, padding='post', truncating='post')
+        X = [X[:, i].astype('int32') for i in range(X.shape[1])]
+
+        asp = _df[self.aspcol].apply(lambda w: [int32(self.w_idx[w])]).values.tolist()
+
+        y = get_dummies(_df[self.clscol].astype(str)).values.astype('float32')
+
+        return X, asp, y
+
+    def batch_gen(self, df):
+        if self.ready is None:
+            self.ready = self.input_ready(df, tokenize=True)
+        X, asp, y = self.ready
+        size = X[0].shape[0]
+        self.n_batchs = size// self.batch_size if size % self.batch_size == 0 else size // self.batch_size + 1
+        current = 0
+        for i in range(self.n_batchs):
+            next_batch = self.batch_size * (i + 1)
+            if i == self.n_batchs - 1:
+                _X = [X[t][current:] for t in range(self.max_seq_len)]
+                _asp = asp[current:]
+                _y = y[current:, :]
+            else:
+                _X = [X[t][current:next_batch] for t in range(self.max_seq_len)]
+                _asp = asp[current:next_batch]
+                _y = y[current:next_batch, :]
+            current = next_batch
+            yield _X, _asp, _y
+
+
+
+
+
