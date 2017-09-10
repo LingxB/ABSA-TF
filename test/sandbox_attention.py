823995712598eae268dbba1923afd79069760b26
ebao <ebao@toolsgroup.com>
2017-09-10 18:41:15 +0200

    finally

--- test/sandbox_attention.py
+++ test/sandbox_attention.py
@@ -13,7 +13,7 @@ dev = pd.read_csv(dev_path)
 dataset = pd.concat([train,test,dev])
 
 
-dm = AttDataManager(batch_size=32)
+dm = AttDataManager(batch_size=25)
 
 dataset = dm.init(dataset)
 
@@ -29,14 +29,15 @@ from copy import deepcopy
 from time import time
 
 num_symbols = dm.vocab+dm.start_idx
-embedding_size = 200 # da
-cell_num = 64 # d
+embedding_size = 300 # da
+cell_num = 300 # d
 dropout_keep_prob = 0.8
-layer_num = 2
+layer_num = 1
 epochs = 25
 seq_len = dm.max_seq_len
-epsilon = 0.003
+epsilon = 0.01
 initializer = tf.random_uniform_initializer(-epsilon, epsilon)
+beta = 0.001 #l2 param
 
 
 
@@ -53,7 +54,8 @@ def feed_dict(X, asp, y):
     return fd
 
 cell = rnn.BasicLSTMCell(cell_num)
-cell = rnn.DropoutWrapper(cell, output_keep_prob=dropout_keep_prob)
+#cell = rnn.LSTMCell(cell_num, initializer=initializer)
+#cell = rnn.DropoutWrapper(cell, output_keep_prob=dropout_keep_prob)
 cells = [deepcopy(cell) for i in range(layer_num)]
 cell = rnn.MultiRNNCell(cells)
 
@@ -65,7 +67,7 @@ with tf.variable_scope('encoder'):
     asp_emb_inputs = tf.nn.embedding_lookup(embedding, asp_inputs)
     enc_output, enc_state = rnn.static_rnn(cell, emb_inputs, dtype='float32')
 
-with tf.name_scope('attention'):
+with tf.variable_scope('attention'):
     H = tf.stack(enc_output, axis=1) #[batch, N, d]
     _H = tf.reshape(tf.stack(enc_output, axis=1), shape=(-1, cell_num)) #[batch*N, d]
     Wh = tf.get_variable('Wh', shape=(cell_num, cell_num), dtype=tf.float32, initializer=initializer) #[d, d]
@@ -90,23 +92,31 @@ with tf.name_scope('attention'):
     hN = enc_output[-1] #[batch, d] TODO: Cehck hN is the last hidden state or last lstm output
 
     h_star = tf.tanh(tf.add(tf.matmul(_r, Wp),tf.matmul(hN, Wx))) #[batch, d]
+    h_star = tf.nn.dropout(h_star, dropout_keep_prob)
 
 
 with tf.variable_scope('output'):
     output = tf.layers.dense(h_star, units=3, name='dense')
     pred = tf.nn.softmax(output, name='softmax')
 
+with tf.name_scope('train_ops'):
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=output))
+
+    #L2 Regularizer
+    reg_params = [param for param in tf.trainable_variables() if not 'embedding:0' in param.name]
+    regularizer = tf.add_n([tf.nn.l2_loss(p) for p in reg_params])
+
+    loss = cross_entropy + beta * regularizer
+
+    train_op = tf.train.AdagradOptimizer(0.01).minimize(loss)
+
+with tf.name_scope('metrics'):
     correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(labels, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))*100
 
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=output))
-train_op = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
-
-
-
-
-
+# TODO: 1. Add L2 regularization using tf.trainable_variables()
+# TODO: 2. Add dropout to h_star, remove dropout between LSTM layers
 
 ##################
 # Debug
@@ -139,14 +149,14 @@ with tf.Session() as sess:
         epoch_loss = []
         epoch_acc = []
         for _X,_asp,_y in generator:
-            _, train_loss, train_acc = sess.run([train_op, cross_entropy, accuracy], feed_dict=feed_dict(_X,_asp,_y))
+            _, train_loss, train_acc = sess.run([train_op, loss, accuracy], feed_dict=feed_dict(_X,_asp,_y))
             epoch_loss.append(train_loss)
             epoch_acc.append(train_acc)
         print('Tain \tloss:%4.8f \tacc:%4.2f%%' % (np.mean(epoch_loss), np.mean(epoch_acc)))
 
         #Testing
         X_, asp_, y_ = dm.input_ready(test, tokenize=True)
-        test_loss, test_acc = sess.run([cross_entropy, accuracy], feed_dict=feed_dict(X_,asp_,y_))
+        test_loss, test_acc = sess.run([loss, accuracy], feed_dict=feed_dict(X_,asp_,y_))
         print('Val \tloss:%4.8f \tacc:%4.2f%%' % (test_loss, test_acc))
 
         end = time()
