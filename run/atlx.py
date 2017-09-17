c4cf5a1b960322d83cd94264e7c86c1703b6ccfe
ebao <ebao@toolsgroup.com>
2017-09-17 22:04:15 +0200

    checkpoint

--- run/atlx.py
+++ run/atlx.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 from src.datamanager import ATLXDataManager
-from src.model.ATLSTM import ATLSTM
+from src.model.ATLXLSTM import ATLXLSTM
 
 lexicon_path = 'data/Lexicon/lexicon_sample500.csv'
 embedding_path = 'data/glove_840B_300d/semeval14_glove.840B.300d.txt'
@@ -18,12 +18,30 @@ dataset = pd.concat([train,test,dev])
 
 dm = ATLXDataManager(batch_size=25)
 
-
-
 dataset = dm.init(dataset, embedding_frame=embedding_frame, lexicon_frame=lexicon_frame)
 
 
-dm.set_max_len(21)
+dm.set_max_len(79)
 
 
-X, asp, lx, y = next(dm.batch_gen(train))
+
+model = ATLXLSTM(datamanager=dm,
+                 embedding_size=300,
+                 aspect_embedding_size=100,
+                 lx_embedding_size=50,
+                 cell_num=300,
+                 layer_num=1, trainable=True)
+
+model.train(train_data=train, epochs=25, val_data=dev)
+
+pred = model.predict(test_data=test)
+test['PRED'] = pred.argmax(axis=1)-1
+test = test.join(pd.DataFrame(pred, columns=['P-1','P0','P+1']))
+test['T/F'] = test.CLS==test.PRED
+test.to_csv(model.model_path+'test_results.csv', index=False)
+
+dev_pred = model.predict(test_data=dev)
+dev['PRED'] = dev_pred.argmax(axis=1)-1
+dev = dev.join(pd.DataFrame(dev_pred, columns=['P-1','P0','P+1']))
+dev['T/F'] = dev.CLS==dev.PRED
+dev.to_csv(model.model_path+'dev_results.csv', index=False)
--- src/model/ATLXLSTM.py
+++ src/model/ATLXLSTM.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
 from src.model.ATLSTM import ATLSTM
+import tensorflow as tf
+from tensorflow.contrib import rnn
+import numpy as np
+import os
+from time import time
+
 
 
 
@@ -39,10 +44,182 @@ class ATLXLSTM(ATLSTM):
             return emb_inputs, asp_emb_inputs, lx_emb_inputs
 
 
+    def _attention_with_lx(self, enc_outputs, asp_emb_inputs, lx_emb_inputs):
+        # Reshape inputs
+        H = tf.stack(enc_outputs, axis=1)  # [batch, N, d]
+        _H = tf.reshape(tf.stack(enc_outputs, axis=1), shape=(-1, self.cell_num))  # [batch*N, d]
+
+        L = tf.stack(lx_emb_inputs, axis=1) # [batch, N, dl]
+        _L = tf.reshape(L, shape=(-1, self.lx_embedding_size)) # [batch*N, dl]
+
+        # Attention variables
+        Wh = tf.get_variable('Wh', shape=(self.cell_num, self.cell_num), dtype=tf.float32,
+                             initializer=self.initializer)  # [d, d]
+        Wv = tf.get_variable('Wv', shape=(self.asp_embedding_size, self.asp_embedding_size), dtype=tf.float32,
+                             initializer=self.initializer)  # [da, da]
+        # add lexicon embedding projection variable
+        Wl = tf.get_variable('Wl', shape=(self.lx_embedding_size, self.lx_embedding_size), dtype=tf.float32,
+                             initializer=self.initializer) # [dl, dl]
+
+        w = tf.get_variable('w', shape=(self.cell_num + self.lx_embedding_size + self.asp_embedding_size, 1), dtype=tf.float32,
+                            initializer=self.initializer)  # [d+da, 1]
+        Wp = tf.get_variable('Wp', shape=(self.cell_num, self.cell_num), dtype=tf.float32,
+                             initializer=self.initializer)  # [d, d]
+        Wx = tf.get_variable('Wx', shape=(self.cell_num, self.cell_num), dtype=tf.float32,
+                             initializer=self.initializer)  # [d, d]
+
+        # Attention operations
+        WhH = tf.reshape(tf.matmul(_H, Wh), (-1, self.seq_len, self.cell_num))  # [batch, N, d]
+        Wvva = tf.reshape(tf.matmul(asp_emb_inputs, Wv), (-1, 1, self.asp_embedding_size))  # [batch, 1, da]
+        WvvaeN = tf.tile(Wvva, (1, self.seq_len, 1))  # [batch, N, da]
+        # add lexicon embedding projection
+        WlL = tf.reshape(tf.matmul(_L, Wl), (-1, self.seq_len, self.lx_embedding_size)) #[batch, N, dl]
+
+        M = tf.tanh(tf.concat([WhH, WlL, WvvaeN], axis=-1))  # [batch, N, d+dl+da]
+        _M = tf.reshape(M, shape=(-1, self.cell_num + self.lx_embedding_size + self.asp_embedding_size))  # [batch*N, d+dl+da]
+
+        alpha = tf.reshape(tf.nn.softmax(tf.matmul(_M, w)), shape=(-1, self.seq_len, 1))  # [batch, N, 1]
+
+        r = tf.matmul(tf.transpose(H, [0, 2, 1]), alpha)  # [batch, d, 1]
+
+        _r = tf.reshape(r, (-1, self.cell_num))  # [batch, d]
+        hN = enc_outputs[-1]  # [batch, d] state.h == output[-1]
+
+        h_star = tf.tanh(tf.add(tf.matmul(_r, Wp), tf.matmul(hN, Wx)))  # [batch, d]
+
+        h_star = tf.nn.dropout(h_star, self.dropout_keep_prob)
+
+        return h_star
+
+    def _feed_dict_with_lx(self, X, asp, lx, y):
+        fd = {self.inputs[t]: X[t] for t in range(self.seq_len)}
+        fd.update({self.asp_inputs: asp})
+        fd.update({self.labels: y})
+        return fd
+
+    def _create_graph_with_lx(self):
+
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            # Placeholders
+            with tf.name_scope('inputs'):
+                self.inputs = [tf.placeholder(tf.int32, shape=(None,), name='inp_token_t%i' % i)
+                               for i in range(self.seq_len)]
+
+                self.asp_inputs = tf.placeholder(tf.int32, shape=(None,), name='aspect_token')
+
+                self.lx_inputs = [tf.placeholder(tf.int32, shape=(None,), name='inp_token_pol_t%i' % i)
+                                  for i in range(self.seq_len)]
+
+                self.labels = tf.placeholder(tf.float32, shape=(None, 3), name='labels')
+
+            # Cell
+            cell = self._create_cell()
+
+            # Embedding
+            emb_inputs, asp_emb_inputs, lx_emb_inputs = self._embedding_with_lx(self.inputs, self.asp_inputs, self.lx_inputs)
+
+            # LSTM encoder
+            enc_outputs, enc_state = rnn.static_rnn(cell, emb_inputs, dtype='float32')
+
+            # Attention
+            h_star = self._attention_with_lx(enc_outputs, asp_emb_inputs, lx_emb_inputs)
+
+            # Output layer
+            with tf.name_scope('output'):
+                output = tf.layers.dense(h_star, units=3, name='dense', kernel_initializer=self.initializer)
+                self.pred = tf.nn.softmax(output, name='softmax')
+
+            # Train ops
+            with tf.name_scope('train_ops'):
+                cross_entropy = tf.reduce_mean(
+                    tf.nn.softmax_cross_entropy_with_logits(labels=self.labels, logits=output))
+
+                # L2 Regularizer
+                reg_params = [param for param in tf.trainable_variables() if not 'embedding' in param.name]
+                # print(reg_params)
+                regularizer = tf.add_n([tf.nn.l2_loss(p) for p in reg_params])
+
+                self.loss = cross_entropy + self.l2_param * regularizer
+                tf.summary.scalar('loss', self.loss)
+
+                self.train_op = self.optimizer.minimize(self.loss)
+
+            with tf.name_scope('metrics'):
+                correct_prediction = tf.equal(tf.argmax(self.pred, 1), tf.argmax(self.labels, 1))
+                self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) * 100
+                tf.summary.scalar('accuracy', self.accuracy)
+
+            summary_weights = [w for w in tf.trainable_variables() if 'embedding' not in w.name]
+            for w in summary_weights:
+                tf.summary.histogram(w.name.strip(':0'), w)
+            self.summary_op = tf.summary.merge_all()
+
+    def train(self, train_data, epochs, val_data=None, verbose=1, **kwargs):
+        self.optimizer = kwargs.get('optimizer', tf.train.AdagradOptimizer(0.01))
+        # Create graph
+        if self.graph is None:
+            self._create_graph_with_lx()
+        # Create save ckpt path
+        if not os.path.exists(self.model_path):
+            os.makedirs(self.model_path)
+        # Write word embedding tsv
+        self.dm.write_embedding_tsv(self.model_path) # TODO: Add polarity tsv
+
+        with tf.Session(graph=self.graph) as sess:
+            # Create train/val writer
+            train_writer = tf.summary.FileWriter(self.model_path + 'train', sess.graph)
+            val_writer = tf.summary.FileWriter(self.model_path + 'val')
+
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            self.saver = tf.train.Saver()
+
+            for epoch in range(epochs):
+                start = time()
+                print('Epoch %i/%i' % (epoch + 1, epochs))
+                # Training
+                generator = self.dm.batch_gen(train_data)
+                epoch_loss = []
+                epoch_acc = []
+                for train_inputs in generator:
+                    _, train_summary, train_loss, train_acc = sess.run([self.train_op, self.summary_op, self.loss, self.accuracy],
+                                                                       feed_dict=self._feed_dict_with_lx(*train_inputs))
+                    epoch_loss.append(train_loss)
+                    epoch_acc.append(train_acc)
+                    train_writer.add_summary(train_summary, epoch * self.dm.n_batchs + 1)
+                    if verbose == 2:
+                        print('\rTain \tloss:%4.8f \tacc:%4.2f%%' % (train_loss, train_acc), end='')
+                if verbose == 2:
+                    print('\n')
+
+                if verbose == 1:
+                    print('Tain \tloss:%4.8f \tacc:%4.2f%%' % (np.mean(epoch_loss), np.mean(epoch_acc)))
+
+                # Testing
+                if val_data is not None:
+                    val_inputs = self.dm.input_ready(val_data, tokenize=True)
+                    val_summary, test_loss, test_acc = sess.run([self.summary_op, self.loss, self.accuracy],
+                                                                feed_dict=self._feed_dict_with_lx(*val_inputs))
+                    val_writer.add_summary(val_summary, epoch * self.dm.n_batchs + 1)
+                    print('Val \tloss:%4.8f \tacc:%4.2f%%' % (test_loss, test_acc))
+
+                end = time()
+                print('Epoch time: %is\n' % (end - start))
+
+            self.saver.save(sess, self.model_path + self.model_name)
+
+
+    def predict(self, test_data, verbose=1):
+        with tf.Session(graph=self.graph) as sess:
+            ckpt = tf.train.get_checkpoint_state(self.model_path)
+            self.saver.restore(sess, ckpt.model_checkpoint_path)
+            test_inputs = self.dm.input_ready(test_data, tokenize=True)
+            test_pred, test_loss, test_acc = sess.run([self.pred, self.loss, self.accuracy],
+                                                      feed_dict=self._feed_dict_with_lx(*test_inputs))
+            if verbose == 1:
+                print('Test \tloss:%4.8f \tacc:%4.2f%%' % (test_loss, test_acc))
+            return test_pred
 
 
 
-
-# TODO: 1. Embed lexicon inputs
-# TODO: 2. Merge lx_emb_inps with enc_output, asp_input
-# TODO: 3. Add lx embeeding tsv in dm
