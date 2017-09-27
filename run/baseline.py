ccc054ed57a978a91c2ec874723002956c6c84c0
ebao <ebao@toolsgroup.com>
2017-09-27 18:42:29 +0200

    add non trainable baseline

--- run/baseline.py
+++ run/baseline.py
@@ -22,7 +22,7 @@ dataset = dm.init(dataset, embedding_frame=embedding_frame)
 
 dm.set_max_len(79)
 
-model = ATLSTM(dm, embedding_size=300, aspect_embedding_size=100, cell_num=300, layer_num=1, trainable=True, seed=726*104)
+model = ATLSTM(dm, embedding_size=300, aspect_embedding_size=100, cell_num=300, layer_num=1, trainable=False, seed=726*104)
 
 model.train(train_data=train, epochs=25, val_data=dev, ramdom_shuffle=True)
 
@@ -44,7 +44,7 @@ train = train.join(pd.DataFrame(train_pred, columns=['P-1','P0','P+1']))
 train['T/F'] = train.CLS==train.PRED
 train.to_csv(model.model_path+'train_results.csv', index=False)
 
-
+# **Trainable = True**
 # Epoch 1/25
 # Tain 	loss:1.30411685 	acc:61.70%
 # Val 	loss:1.27498603 	acc:61.93%
@@ -147,5 +147,110 @@ train.to_csv(model.model_path+'train_results.csv', index=False)
 # Epoch time: 67s
 # INFO:tensorflow:Restoring parameters from ./models/Wed_Sep_27_15-53-34_2017/ATLSTM
 # Test 	loss:0.86237544 	acc:81.60%
+
+# **Trainable = False**
+# Epoch 1/25
+# Tain 	loss:1.30414844 	acc:61.70%
+# Val 	loss:1.27511978 	acc:61.93%
+# Epoch time: 73s
+# Epoch 2/25
+# Tain 	loss:1.20999849 	acc:64.32%
+# Val 	loss:1.21358609 	acc:62.88%
+# Epoch time: 70s
+# Epoch 3/25
+# Tain 	loss:1.14242935 	acc:67.04%
+# Val 	loss:1.10861969 	acc:67.23%
+# Epoch time: 73s
+# Epoch 4/25
+# Tain 	loss:1.10033619 	acc:69.91%
+# Val 	loss:1.10692239 	acc:67.99%
+# Epoch time: 75s
+# Epoch 5/25
+# Tain 	loss:1.06994247 	acc:71.58%
+# Val 	loss:1.06943262 	acc:70.27%
+# Epoch time: 72s
+# Epoch 6/25
+# Tain 	loss:1.04764605 	acc:72.89%
+# Val 	loss:1.04396021 	acc:72.35%
+# Epoch time: 73s
+# Epoch 7/25
+# Tain 	loss:1.03073895 	acc:73.53%
+# Val 	loss:1.05348337 	acc:71.59%
+# Epoch time: 66s
+# Epoch 8/25
+# Tain 	loss:1.01303065 	acc:74.47%
+# Val 	loss:1.02873290 	acc:72.16%
+# Epoch time: 70s
+# Epoch 9/25
+# Tain 	loss:1.00410056 	acc:74.40%
+# Val 	loss:1.04548872 	acc:72.73%
+# Epoch time: 53s
+# Epoch 10/25
+# Tain 	loss:0.98539060 	acc:75.52%
+# Val 	loss:1.01037180 	acc:72.35%
+# Epoch time: 49s
+# Epoch 11/25
+# Tain 	loss:0.97539955 	acc:75.50%
+# Val 	loss:1.01852953 	acc:72.73%
+# Epoch time: 50s
+# Epoch 12/25
+# Tain 	loss:0.96209389 	acc:75.61%
+# Val 	loss:1.01759839 	acc:72.16%
+# Epoch time: 50s
+# Epoch 13/25
+# Tain 	loss:0.95111847 	acc:76.07%
+# Val 	loss:0.99396014 	acc:71.97%
+# Epoch time: 49s
+# Epoch 14/25
+# Tain 	loss:0.94154173 	acc:76.38%
+# Val 	loss:1.00140095 	acc:72.92%
+# Epoch time: 49s
+# Epoch 15/25
+# Tain 	loss:0.93234867 	acc:76.53%
+# Val 	loss:0.99375677 	acc:73.86%
+# Epoch time: 48s
+# Epoch 16/25
+# Tain 	loss:0.92585552 	acc:76.32%
+# Val 	loss:0.98094702 	acc:72.92%
+# Epoch time: 47s
+# Epoch 17/25
+# Tain 	loss:0.91453832 	acc:77.33%
+# Val 	loss:0.98515534 	acc:72.54%
+# Epoch time: 46s
+# Epoch 18/25
+# Tain 	loss:0.90541703 	acc:77.47%
+# Val 	loss:0.97396755 	acc:72.92%
+# Epoch time: 46s
+# Epoch 19/25
+# Tain 	loss:0.89725214 	acc:77.48%
+# Val 	loss:0.97023869 	acc:72.16%
+# Epoch time: 46s
+# Epoch 20/25
+# Tain 	loss:0.88328540 	acc:77.99%
+# Val 	loss:0.97458839 	acc:73.86%
+# Epoch time: 47s
+# Epoch 21/25
+# Tain 	loss:0.87975860 	acc:78.21%
+# Val 	loss:0.98558789 	acc:74.62%
+# Epoch time: 47s
+# Epoch 22/25
+# Tain 	loss:0.86942965 	acc:78.27%
+# Val 	loss:0.97063738 	acc:72.35%
+# Epoch time: 46s
+# Epoch 23/25
+# Tain 	loss:0.86492103 	acc:78.44%
+# Val 	loss:0.96943516 	acc:73.67%
+# Epoch time: 47s
+# Epoch 24/25
+# Tain 	loss:0.86024052 	acc:78.80%
+# Val 	loss:0.96429104 	acc:73.67%
+# Epoch time: 47s
+# Epoch 25/25
+# Tain 	loss:0.85301620 	acc:78.68%
+# Val 	loss:0.97546113 	acc:74.05%
+# Epoch time: 48s
+# INFO:tensorflow:Restoring parameters from ./models/Wed_Sep_27_18-11-04_2017/ATLSTM
+# Test 	loss:0.84097284 	acc:80.27%
+
 
 
